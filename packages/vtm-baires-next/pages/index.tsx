9dc527ffdabfc79bb38c2b2ce0ac4908d0af05f8
MaxDac <massimiliano.dacunzo@hotmail.com>
2022-03-02 13:18:34 +0000

    Update index.tsx

--- packages/vtm-baires-next/pages/index.tsx
+++ packages/vtm-baires-next/pages/index.tsx
@@ -2,11 +2,10 @@ import {useCustomLazyLoadQuery} from "vtm-baires-next-utils";
 import {clansQuery} from "vtm-baires-next-services/graphql-queries/queries/info/ClansQuery";
 import {ClansQuery} from "vtm-baires-next-services/graphql-queries/queries/info/__generated__/ClansQuery.graphql";
 
-const Index = (props: any) => {
+export default function Index(props: any) {
     const clans = useCustomLazyLoadQuery<ClansQuery>(clansQuery, {})
+
     return (
         <div>Test</div>
     )
 }
-
-export default Index
